d0d264dd6e0d3dc43df4a15b79ff2a3ba9b0917b
Abstract-ak <ash.king0205@gmail.com>
2025-01-05 14:44:48 +0530

    routes done

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,21 @@
 // ? (Route definition)
 
-const express = require('express');
+const express = require("express");
+const {
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
 
 const router = express.Router();
 
-
+//CRUD operations
+router.get("/", getTasks);
+router.get("/:id", getTaskById);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 module.exports = router;
